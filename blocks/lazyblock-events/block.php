99afab00a7ee88dcd842027fa84f33579139d235
Jeremy Pittard <jeremy@jpittard.net>
2023-10-31 14:52:44 +0800

    polish, improved events section, improved people section

--- blocks/lazyblock-events/block.php
+++ blocks/lazyblock-events/block.php
@@ -9,7 +9,7 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
-$query = get_posts(array('post_type' => 'event', 'posts_per_page' => -1));
+$query = get_posts(array('post_type' => 'event', 'posts_per_page' => -1, 'orderby' => 'meta_value', 'meta_key' => 'date', 'meta_type' => 'DATETIME', 'order' => 'ASC'));
 $context = Timber::context();
 $context['events'] = $query;
 $context['events_to_show'] =  $attributes['events-to-show'];
